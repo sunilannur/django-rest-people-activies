6e689f3f1e005f3d714ac2c3d61dd1d53ad21532
hemanth <harshahemanth007@gmail.com>
2020-05-06 23:40:41 +0530

    final

--- member/serializers.py
+++ member/serializers.py
@@ -4,9 +4,20 @@ from .models import ActivityPeriod, User
 
 class ActivityPeriodSerializer(serializers.ModelSerializer):
 
+    start_time = serializers.SerializerMethodField()
+    end_time = serializers.SerializerMethodField()
+
     class Meta:
         model = ActivityPeriod
         fields = ('start_time', 'end_time')
+
+    def get_start_time(self, obj):
+        format = '%Y-%m-%d %H:%M %p'
+        return obj.start_time.strftime(format)
+    
+    def get_end_time(self, obj):
+        format = '%Y-%m-%d %H:%M %p'
+        return obj.end_time.strftime(format)
 
 
 class UserSerializer(serializers.ModelSerializer):
